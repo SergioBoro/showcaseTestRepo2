a511f380cf0101723c5fb3f4d18a00aa61cff270
d.orekhov <orekhoff@gmail.com>
2011-10-27 17:12:56 +0000

    правки связанные с аспектами
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@2336 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/ru/curs/showcase/util/exception/BaseException.java
+++ src/ru/curs/showcase/util/exception/BaseException.java
@@ -1,5 +1,7 @@
 package ru.curs.showcase.util.exception;
 
+import org.slf4j.*;
+
 import ru.curs.showcase.app.api.ExceptionType;
 import ru.curs.showcase.app.api.datapanel.DataPanelElementContext;
 
@@ -21,6 +23,10 @@ public abstract class BaseException extends RuntimeException {
 
 	private static final long serialVersionUID = 6824298988092385401L;
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(BaseException.class);
+
+	private static final String ERROR_CAPTION = "Сообщение об ошибке";
+
 	/**
 	 * Тип исключения.
 	 */
@@ -35,21 +41,36 @@ public abstract class BaseException extends RuntimeException {
 	public BaseException(final ExceptionType aType, final Throwable aCause) {
 		super(aCause);
 		type = aType;
+		logAll(this);
 	}
 
 	public BaseException(final ExceptionType aType) {
 		super();
 		type = aType;
+		logAll(this);
 	}
 
 	public BaseException(final ExceptionType aType, final String aMessage) {
 		super(aMessage);
 		type = aType;
+		logAll(this);
 	}
 
 	public BaseException(final ExceptionType aType, final String aMessage, final Throwable aCause) {
 		super(aMessage, aCause);
 		type = aType;
+		logAll(this);
+	}
+
+	/**
+	 * Выводит в лог полную информацию об исключении.
+	 * 
+	 * @param e
+	 *            - исключение.
+	 */
+	public final void logAll(final Throwable e) {
+		String formatedMes = ERROR_CAPTION;
+		LOGGER.error(formatedMes, e);
 	}
 
 	public ExceptionType getType() {
