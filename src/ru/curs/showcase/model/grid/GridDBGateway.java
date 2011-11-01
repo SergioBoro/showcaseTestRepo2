03319609001aa971fe7d8a0defc9d1c4a5c74d3a
d.orekhov <orekhoff@gmail.com>
2011-11-01 18:02:58 +0000

    закончил перевод экспорта карты на команды + новые тестовые данные
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@2358 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/ru/curs/showcase/model/grid/GridDBGateway.java
+++ src/ru/curs/showcase/model/grid/GridDBGateway.java
@@ -9,7 +9,7 @@ import ru.curs.showcase.app.api.event.CompositeContext;
 import ru.curs.showcase.app.api.grid.GridContext;
 import ru.curs.showcase.model.*;
 import ru.curs.showcase.runtime.*;
-import ru.curs.showcase.util.OutputStreamDataFile;
+import ru.curs.showcase.util.*;
 
 /**
  * Шлюз к БД для грида.
