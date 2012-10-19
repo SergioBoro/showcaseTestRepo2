ec0eac717c8698308b49c939e493a77060857302
Andrey <andrey@curs.ru>
2012-10-19 19:00:49 +0000

    Данные о ширине и высоте передаются в sessioncontext в процедуру метадата грида
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@2986 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/ru/curs/showcase/core/grid/LiveGridMetadataGetCommand.java
+++ src/ru/curs/showcase/core/grid/LiveGridMetadataGetCommand.java
@@ -1,8 +1,11 @@
 package ru.curs.showcase.core.grid;
 
+import org.w3c.dom.*;
+
 import ru.curs.showcase.app.api.datapanel.*;
 import ru.curs.showcase.app.api.grid.*;
 import ru.curs.showcase.core.command.*;
+import ru.curs.showcase.util.xml.XMLUtils;
 
 /**
  * Команда получения метаданных для LiveGrid.
@@ -32,9 +35,26 @@ public class LiveGridMetadataGetCommand extends DataPanelElementCommand<LiveGrid
 	 **/
 	@Override
 	protected void mainProc() throws Exception {
+		includeProposedWidthAndHeightInSessionContext();
 		GridGetCommand command = new GridGetCommand(getContext(), getElementInfo(), true);
 		Grid grid = command.execute();
 		setResult(GridTransformer.gridToLiveGridMetadata(grid));
+	}
+
+	private void includeProposedWidthAndHeightInSessionContext() throws Exception {
+
+		Document doc = XMLUtils.stringToDocument(getContext().getSession());
+
+		Element node = doc.createElement("proposedGridWidth");
+		doc.getDocumentElement().appendChild(node);
+		node.appendChild(doc.createTextNode(getContext().getProposedGridWidth().toString()));
+
+		node = doc.createElement("proposedGridHeight");
+		doc.getDocumentElement().appendChild(node);
+		node.appendChild(doc.createTextNode(getContext().getProposedGridHeight().toString()));
+
+		String result = XMLUtils.documentToString(doc);
+		getContext().setSession(result);
 	}
 
 }
