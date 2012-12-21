070fd26192ce150e518c02a49bd358874f3c3684
Andrey <andrey@curs.ru>
2012-12-21 18:04:23 +0000

    fix#522
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@3060 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/ru/curs/showcase/app/client/MessageBox.java
+++ src/ru/curs/showcase/app/client/MessageBox.java
@@ -67,7 +67,7 @@ public final class MessageBox {
 	/**
 	 * 
 	 * Функция создания окна DialogBox, которое закрывается горячей клавишей
-	 * ESC. (test).
+	 * ESC. (test_).
 	 * 
 	 * @return возвращает DialogBox
 	 * 
