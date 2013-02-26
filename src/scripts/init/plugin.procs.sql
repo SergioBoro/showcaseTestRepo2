e7ac1330fa3874f63a6a0c96c687118068012729
a.bogatov <a.bogatov@curs.ru>
2013-02-26 10:49:14 +0000

    Пример получение данных с использованием строки поиска
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@3131 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/scripts/init/plugin.procs.sql
+++ src/scripts/init/plugin.procs.sql
@@ -12,14 +12,26 @@ CREATE PROCEDURE [dbo].[extJsTree_getData]
 AS
 BEGIN
 declare
-	@parentId nvarchar(20)=''
+	@parentId nvarchar(20)='',
+	@curValue nvarchar(20)=''
 	
 set @parentId = @params.value('(/params/id)[1]','nvarchar(20)');
+if @parentId is null begin 
+	set @parentId = 'Root'
+end
+set @curValue = @params.value('(/params/curValue)[1]','nvarchar(20)');
+if @curValue is null 
+	begin 
+		set @curValue = ''
+	end else 
+	begin
+		set @curValue = '['+@curValue+']'
+	end
 
 set @data='
 	<items>
-		<item id="'+@parentId+'.1" name="Lazy loaded item '+@parentId+'.1" leaf="false"/>
-		<item id="'+@parentId+'.2" name="Lazy loaded item '+@parentId+'.2" leaf="false"/>
+		<item id="'+@parentId+'.1" name="Lazy loaded item '+@parentId+'.1'+@curValue+'" leaf="false"/>
+		<item id="'+@parentId+'.2" name="Lazy loaded item '+@parentId+'.2'+@curValue+'" leaf="false"/>
     </items>
 	';
 
