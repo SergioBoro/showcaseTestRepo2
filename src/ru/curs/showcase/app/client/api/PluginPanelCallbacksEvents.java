7942b96b8b5e9fa030e32ab44c780953f65f37ee
a.bogatov <a.bogatov@curs.ru>
2013-03-01 08:26:23 +0000

    fix ошибки формирование xml из JSON в IE
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@3136 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/ru/curs/showcase/app/client/api/PluginPanelCallbacksEvents.java
+++ src/ru/curs/showcase/app/client/api/PluginPanelCallbacksEvents.java
@@ -203,31 +203,31 @@ public final class PluginPanelCallbacksEvents {
 
 	private static String createXmlByJSONValue(final String name, final JSONValue jsonVal) {
 		StringBuilder sb = new StringBuilder();
-		if (jsonVal.isNull() == null) {			
-			if (jsonVal.isObject() != null) {
-				sb.append("<").append(name).append(">");
-				JSONObject jsonObject = jsonVal.isObject();
-				for (String key : jsonObject.keySet()) {
-					sb.append(createXmlByJSONValue(key, jsonObject.get(key)));
-				}
-				sb.append("</").append(name).append(">");
-			} else if (jsonVal.isArray() != null) {
-				JSONArray jsonArray = jsonVal.isArray();
-				for (int i = 0; i < jsonArray.size(); i++) {
-					sb.append(createXmlByJSONValue(name, jsonArray.get(i)));
-				}
+		if (jsonVal == null || jsonVal.isNull() != null) {
+			sb.append("<").append(name).append(">").append("</").append(name).append(">");
+		} else if (jsonVal.isObject() != null) {
+			sb.append("<").append(name).append(">");
+			JSONObject jsonObject = jsonVal.isObject();
+			for (String key : jsonObject.keySet()) {
+				sb.append(createXmlByJSONValue(key, jsonObject.get(key)));
+			}
+			sb.append("</").append(name).append(">");
+		} else if (jsonVal.isArray() != null) {
+			JSONArray jsonArray = jsonVal.isArray();
+			for (int i = 0; i < jsonArray.size(); i++) {
+				sb.append(createXmlByJSONValue(name, jsonArray.get(i)));
+			}
+		} else {
+			sb.append("<").append(name).append(">");
+			String val;
+			if (jsonVal.isString() != null) {
+				JSONString jsonString = jsonVal.isString();
+				val = jsonString.stringValue();
 			} else {
-				sb.append("<").append(name).append(">");
-				String val;
-				if (jsonVal.isString() != null) {
-					JSONString jsonString = jsonVal.isString();
-					val = jsonString.stringValue();
-				} else {
-					val = jsonVal.toString();
-				}
-				sb.append(val);
-				sb.append("</").append(name).append(">");
-			}			
+				val = jsonVal.toString();
+			}
+			sb.append(val);
+			sb.append("</").append(name).append(">");
 		}
 		return sb.toString();
 	}
