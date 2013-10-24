7f711a6a8324526835174dbe73ceb9f1f8ffa0f3
Andrey <andrey@curs.ru>
2013-10-24 15:34:15 +0000

    internationlization
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@3289 322482b4-72a2-ae44-8c81-ef1a4f415bef

---
+++ src/ru/curs/showcase/app/client/internationalization/constantsShowcase.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ru.curs.showcase.app.client.internationalization;
+
+import com.google.gwt.i18n.client.Messages;
+
+/**
+ * @author a.lugovtsov
+ * 
+ */
+
+public interface constantsShowcase extends Messages {
+
+	@DefaultMessage("sendButtonText")
+	String sendButtonText();
+
+}
---
+++ src/ru/curs/showcase/app/client/internationalization/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author a.lugovtsov
+ *
+ */
+package ru.curs.showcase.app.client.internationalization;
