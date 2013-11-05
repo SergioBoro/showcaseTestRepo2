db8eed2726e200e8da65845c12fd25b85af44efe
Andrey <andrey@curs.ru>
2013-11-05 11:46:16 +0000

    РЕФАКТОРИНГ
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@3315 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/ru/curs/showcase/core/celesta/CelestaHelper.java
+++ src/ru/curs/showcase/core/celesta/CelestaHelper.java
@@ -2,7 +2,7 @@ package ru.curs.showcase.core.celesta;
 
 import org.python.core.PyObject;
 
-import ru.curs.celesta.Celesta;
+import ru.curs.celesta.*;
 import ru.curs.showcase.app.api.event.CompositeContext;
 import ru.curs.showcase.runtime.SessionUtils;
 import ru.curs.showcase.util.xml.XMLUtils;
@@ -33,8 +33,7 @@ public class CelestaHelper<T> {
 	 * @param aResultType
 	 *            тип возвращаемого результата.
 	 */
-	public CelestaHelper(final CompositeContext oContex,
-			final Class<T> aResultType) {
+	public CelestaHelper(final CompositeContext oContex, final Class<T> aResultType) {
 		super();
 		this.contex = oContex;
 		this.resultType = aResultType;
@@ -48,49 +47,45 @@ public class CelestaHelper<T> {
 	 * @return результат выполнения скрипта
 	 */
 	public T runPython(final String sProcName, final Object... additionalParams) {
-		Object[] params = mergeAddAndGeneralParameters(this.contex,
-				additionalParams);
+		Object[] params = mergeAddAndGeneralParameters(this.contex, additionalParams);
 		String userSID = SessionUtils.getCurrentUserSID();
 		String procName = CelestaUtils.getRealProcName(sProcName);
+		PyObject result;
 		try {
-			PyObject result = Celesta.getInstance().runPython(userSID,
-					procName, params);
-			if (result == null) {
-				return null;
-			}
-			Object obj = result.__tojava__(resultType);
-			if (obj == null) {
-				return null;
-			}
-			if (obj.getClass().isAssignableFrom(resultType)) {
-				return resultType.cast(obj);
-			} else {
-				throw new CelestaWorkerException("Result is not instance of "
-						+ this.resultType.getName());
-			}
-		} catch (Exception ex) {
-			throw new CelestaWorkerException("Error run celesta jython script",
-					ex);
+			result = Celesta.getInstance().runPython(userSID, procName, params);
+		} catch (CelestaException ex) {
+			throw new CelestaWorkerException("Error run celesta jython script", ex);
 		}
+		if (result == null) {
+			return null;
+		}
+		Object obj = result.__tojava__(resultType);
+		if (obj == null) {
+			return null;
+		}
+		if (obj.getClass().isAssignableFrom(resultType)) {
+			return resultType.cast(obj);
+		} else {
+			throw new CelestaWorkerException("Result is not instance of "
+					+ this.resultType.getName());
+		}
+
 	}
 
-	protected Object[] mergeAddAndGeneralParameters(
-			final CompositeContext context, final Object[] additionalParams) {
+	protected Object[] mergeAddAndGeneralParameters(final CompositeContext context,
+			final Object[] additionalParams) {
 		Object[] resultParams;
 		if (additionalParams != null && additionalParams.length > 0) {
-			resultParams = new Object[additionalParams.length
-					+ LENGTH_GENERAL_PARAMS];
-			System.arraycopy(additionalParams, 0, resultParams,
-					LENGTH_GENERAL_PARAMS, additionalParams.length);
+			resultParams = new Object[additionalParams.length + LENGTH_GENERAL_PARAMS];
+			System.arraycopy(additionalParams, 0, resultParams, LENGTH_GENERAL_PARAMS,
+					additionalParams.length);
 		} else {
 			resultParams = new Object[LENGTH_GENERAL_PARAMS];
 		}
 		resultParams[MAIN_CONTEXT_INDEX] = context.getMain();
 		resultParams[ADD_CONTEXT_INDEX] = context.getAdditional();
-		resultParams[FILTER_CONTEXT_INDEX] = XMLUtils.convertXmlToJson(context
-				.getFilter());
-		resultParams[SESSION_CONTEXT_INDEX] = XMLUtils.convertXmlToJson(context
-				.getSession());
+		resultParams[FILTER_CONTEXT_INDEX] = XMLUtils.convertXmlToJson(context.getFilter());
+		resultParams[SESSION_CONTEXT_INDEX] = XMLUtils.convertXmlToJson(context.getSession());
 		return resultParams;
 	}
 }
