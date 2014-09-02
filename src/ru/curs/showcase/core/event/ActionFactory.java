c14be57acc1fcfcc9d5504e8a4479093b28e9246
Andrey <andrey@curs.ru>
2014-09-02 17:55:56 +0000

    Тестовые правки по оптимизации загрузки навигатора (отключены проверки actions висящих на элементов навигатора). Правки сделаны для тестов в РНИМУ
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@3528 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/ru/curs/showcase/core/event/ActionFactory.java
+++ src/ru/curs/showcase/core/event/ActionFactory.java
@@ -3,7 +3,6 @@ package ru.curs.showcase.core.event;
 import org.xml.sax.Attributes;
 
 import ru.curs.showcase.app.api.event.*;
-import ru.curs.showcase.core.*;
 import ru.curs.showcase.util.xml.*;
 
 /**
@@ -185,11 +184,22 @@ public class ActionFactory extends SAXTagHandler {
 		DataPanelLink curDataPanelLink = new DataPanelLink();
 		curDataPanelLink.setDataPanelId(attrs.getValue(DP_ID_ATTR_NAME));
 
-		ActionTabFinder finder = AppRegistry.getActionTabFinder();
+		// ActionTabFinder finder = AppRegistry.getActionTabFinder();
+		// CompositeContext context = curAction.getContext().gwtClone();
+		// context.setSession(callContext.getSession());
+		// curDataPanelLink.setTabId(finder.findTabForAction(context,
+		// curDataPanelLink,
+		// attrs.getValue(TAB_TAG)));
+
+		// ActionTabFinder finder = AppRegistry.getActionTabFinder();
 		CompositeContext context = curAction.getContext().gwtClone();
 		context.setSession(callContext.getSession());
-		curDataPanelLink.setTabId(finder.findTabForAction(context, curDataPanelLink,
-				attrs.getValue(TAB_TAG)));
+
+		curDataPanelLink.setTabId(attrs.getValue(TAB_TAG));
+		// curDataPanelLink.setTabId(finder.findTabForAction(context,
+		// curDataPanelLink,
+		// attrs.getValue(TAB_TAG)));
+
 		curAction.setDataPanelLink(curDataPanelLink);
 	}
 
