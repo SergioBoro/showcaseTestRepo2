2a04f449c4094abf15e332f4c3993d75f0f1a19c
s.borodanev <s.borodanev@curs.ru>
2014-12-11 10:54:04 +0000

    Добавлен запуск jython-скриптов при наступлении событий Activiti.
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@3633 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/ru/curs/showcase/activiti/EventHandlerForActiviti.java
+++ src/ru/curs/showcase/activiti/EventHandlerForActiviti.java
@@ -1,15 +1,21 @@
 package ru.curs.showcase.activiti;
 
+import java.io.*;
 import java.util.Random;
+import java.util.regex.*;
 
 import org.activiti.engine.delegate.event.*;
-import org.python.core.PyObject;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.python.core.*;
+import org.python.util.PythonInterpreter;
 import org.slf4j.*;
 
 import ru.curs.celesta.*;
 import ru.curs.showcase.app.api.ExceptionType;
-import ru.curs.showcase.runtime.AppInfoSingleton;
-import ru.curs.showcase.util.exception.BaseException;
+import ru.curs.showcase.core.jython.*;
+import ru.curs.showcase.runtime.*;
+import ru.curs.showcase.util.TextUtils;
+import ru.curs.showcase.util.exception.*;
 
 /**
  * 
@@ -19,6 +25,10 @@ import ru.curs.showcase.util.exception.BaseException;
  * 
  */
 public class EventHandlerForActiviti implements ActivitiEventListener {
+	public static final String JYTHON_MARKER = "jython";
+	private static final String JYTHON_ERROR =
+		"При вызове Jython процедуры '%s' произошла ошибка: %s";
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(EventHandlerForActiviti.class);
 
 	private class MyException extends BaseException {
@@ -50,56 +60,147 @@ public class EventHandlerForActiviti implements ActivitiEventListener {
 				String procName =
 					AppInfoSingleton.getAppInfo().getActivitiEventScriptDictionary().get(aet);
 
-				final int i3 = 3;
-				final int i8 = 8;
-				if (procName.endsWith(".cl")) {
-					procName = procName.substring(0, procName.length() - i3);
-
-				}
-				if (procName.endsWith(".celesta")) {
-					procName = procName.substring(0, procName.length() - i8);
-				}
-
-				String tempSesId = String.format("Celesta%08X", (new Random()).nextInt());
-				try {
-					Celesta.getInstance().login(tempSesId, "super");
-					PyObject pObj = Celesta.getInstance().runPython(tempSesId, procName, event);
-					Object obj = pObj.__tojava__(Object.class);
-				} catch (CelestaException e) {
-					if (e.getMessage().contains("Traceback")) {
-						int ind = e.getMessage().indexOf("Traceback");
-						String ex = e.getMessage().substring(0, ind - 1).trim();
-						throw new MyException(ExceptionType.SOLUTION,
-								"При запуске процедуры Celesta произошла ошибка: " + ex);
-					} else {
-						throw new MyException(ExceptionType.SOLUTION,
-								"При запуске процедуры Celesta произошла ошибка: "
-										+ e.getMessage());
-					}
-
-				} finally {
-					try {
-						Celesta.getInstance().logout(tempSesId, false);
-					} catch (CelestaException e) {
-						if (e.getMessage().contains("Traceback")) {
-							int ind = e.getMessage().indexOf("Traceback");
-							String ex = e.getMessage().substring(0, ind - 1).trim();
-							throw new MyException(ExceptionType.SOLUTION,
-									"При запуске процедуры Celesta произошла ошибка: " + ex);
-						} else {
-							throw new MyException(ExceptionType.SOLUTION,
-									"При запуске процедуры Celesta произошла ошибка: "
-											+ e.getMessage());
-						}
-					}
+				if (procName.endsWith(".cl") || procName.endsWith(".celesta")) {
+					LOGGER.info("Выполняется celesta-скрипт " + procName);
+					handleCelestaScript(procName, event);
+				} else if (procName.endsWith(".py")) {
+					LOGGER.info("Выполняется jython-скрипт " + procName);
+					handlePythonScript(procName, event);
 				}
 			}
 		}
-
 	}
 
 	@Override
 	public boolean isFailOnException() {
 		return false;
+	}
+
+	private void handleCelestaScript(String procName, ActivitiEvent event) {
+		final int i3 = 3;
+		final int i8 = 8;
+		if (procName.endsWith(".cl")) {
+			procName = procName.substring(0, procName.length() - i3);
+
+		}
+		if (procName.endsWith(".celesta")) {
+			procName = procName.substring(0, procName.length() - i8);
+		}
+
+		String tempSesId = String.format("Celesta%08X", (new Random()).nextInt());
+		try {
+			Celesta.getInstance().login(tempSesId, "super");
+			PyObject pObj = Celesta.getInstance().runPython(tempSesId, procName, event);
+			Object obj = pObj.__tojava__(Object.class);
+		} catch (CelestaException e) {
+			if (e.getMessage().contains("Traceback")) {
+				int ind = e.getMessage().indexOf("Traceback");
+				String ex = e.getMessage().substring(0, ind - 1).trim();
+				throw new MyException(ExceptionType.SOLUTION,
+						"При запуске процедуры Celesta произошла ошибка: " + ex);
+			} else {
+				throw new MyException(ExceptionType.SOLUTION,
+						"При запуске процедуры Celesta произошла ошибка: " + e.getMessage());
+			}
+
+		} finally {
+			try {
+				Celesta.getInstance().logout(tempSesId, false);
+			} catch (CelestaException e) {
+				if (e.getMessage().contains("Traceback")) {
+					int ind = e.getMessage().indexOf("Traceback");
+					String ex = e.getMessage().substring(0, ind - 1).trim();
+					throw new MyException(ExceptionType.SOLUTION,
+							"При запуске процедуры Celesta произошла ошибка: " + ex);
+				} else {
+					throw new MyException(ExceptionType.SOLUTION,
+							"При запуске процедуры Celesta произошла ошибка: " + e.getMessage());
+				}
+			}
+		}
+	}
+
+	private void handlePythonScript(String procName, ActivitiEvent event) {
+		PythonInterpreter interpreter = JythonIterpretatorFactory.getInstance().acquire();
+		String parent = procName.replaceAll("([.]\\w+)$", "");
+		parent = parent.replace('/', '.');
+		boolean isLoaded = false;
+		try {
+			String className = TextUtils.extractFileName(procName);
+			File script =
+				new File(JythonIterpretatorFactory.getUserDataScriptDir() + "/" + procName);
+			if (!script.exists()) {
+				script =
+					new File(JythonIterpretatorFactory.getGeneralScriptDir() + "/" + procName);
+				if (!script.exists()) {
+					throw new SettingsFileOpenException(procName, SettingsFileType.JYTHON);
+				}
+			}
+			String cmd =
+				String.format(
+						"from org.python.core import codecs; codecs.setDefaultEncoding('utf-8'); from %s import %s",
+						parent, className);
+			try {
+				setupJythonLogging(interpreter);
+				interpreter.exec(cmd);
+				isLoaded = true;
+
+				interpreter.set("event", event);
+				PyObject pyClass = interpreter.get(className);
+				PyObject pyObj = pyClass.__call__();
+			} catch (PyException e) {
+				JythonWrongClassException exc =
+					JythonWrongClassException.checkForImportError(e.toString(), className);
+				if (exc != null) {
+					throw exc;
+				}
+
+				String error = handleJythonException(e.value.toString());
+				throw new JythonException(String.format(JYTHON_ERROR, procName, error), e);
+			}
+		} finally {
+			if (isLoaded) {
+				interpreter.exec("import sys; del sys.modules['" + parent + "']");
+			}
+			JythonIterpretatorFactory.getInstance().release(interpreter);
+		}
+
+	}
+
+	private void setupJythonLogging(final PythonInterpreter interpreter) {
+		interpreter.setOut(new Writer() {
+
+			@Override
+			public void write(final char[] data, final int offset, final int count)
+					throws IOException {
+				if (AppInfoSingleton.getAppInfo().isEnableLogLevelInfo()) {
+					String value = String.valueOf(data, offset, count);
+					if (!value.trim().isEmpty()) {
+						Marker marker = MarkerFactory.getDetachedMarker(JYTHON_MARKER);
+						LOGGER.info(marker, value);
+					}
+				}
+			}
+
+			@Override
+			public void flush() throws IOException {
+				// ничего не делаем
+			}
+
+			@Override
+			public void close() throws IOException {
+				// ничего не делаем
+			}
+		});
+	}
+
+	private String handleJythonException(final String value) {
+		String error = StringEscapeUtils.unescapeJava(value);
+		Pattern regex = Pattern.compile("^Exception\\(u'(.+)*',\\)$", Pattern.MULTILINE);
+		Matcher regexMatcher = regex.matcher(error);
+		if (regexMatcher.matches()) {
+			return regexMatcher.group(1);
+		}
+		return error;
 	}
 }
