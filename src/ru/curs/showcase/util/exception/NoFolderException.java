1a5e9927124f75a2810ee6689b3342c2ab19cd80
s.borodanev <s.borodanev@curs.ru>
2016-01-28 12:39:45 +0000

    Установлена проверка на отсутствие системной библиотеки common.sys
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@4148 322482b4-72a2-ae44-8c81-ef1a4f415bef

---
+++ src/ru/curs/showcase/util/exception/NoFolderException.java
@@ -0,0 +1,24 @@
+package ru.curs.showcase.util.exception;
+
+public class NoFolderException extends RuntimeException {
+	private static final String ERROR_MES =
+		"Userdatas folder does not consist '%s' system library";
+
+	private static final long serialVersionUID = -7664887404313831091L;
+
+	/**
+	 * Наименование директории
+	 */
+
+	private final String directory;
+
+	public String getDirectory() {
+		return directory;
+	}
+
+	public NoFolderException(final String directory) {
+		super(String.format(ERROR_MES, directory));
+		this.directory = directory;
+	}
+
+}
