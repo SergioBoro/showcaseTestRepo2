5e9a273c8c305206509351a2b5450f01741eb3be
Boris <boris@curs.ru>
2017-04-06 13:23:37 +0000

    Сделал, чтобы в селекторах вместо кодов специальных html символов отображались сами символы (fix #1283)
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@4633 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/ru/curs/showcase/core/selector/SelectorCelestaGateway.java
+++ src/ru/curs/showcase/core/selector/SelectorCelestaGateway.java
@@ -1,8 +1,9 @@
 package ru.curs.showcase.core.selector;
 
 import ru.curs.showcase.app.api.event.CompositeContext;
-import ru.curs.showcase.app.api.selector.DataRequest;
+import ru.curs.showcase.app.api.selector.*;
 import ru.curs.showcase.core.celesta.CelestaHelper;
+import ru.curs.showcase.util.xml.XMLUtils;
 
 /**
  * Шлюз для получения данных, источником которых являются celesta.
@@ -18,6 +19,7 @@ public class SelectorCelestaGateway implements SelectorGateway {
 		CelestaHelper<ResultSelectorData> helper =
 			new CelestaHelper<ResultSelectorData>(context, ResultSelectorData.class);
 
+		ResultSelectorData result = null;
 		String procName = req.getProcName();
 		if (procName.indexOf(Constants.PROCNAME_SEPARATOR) != -1) {
 			// загрузка данных в 2 этапа (выполнение 2-х скриптов)
@@ -34,15 +36,27 @@ public class SelectorCelestaGateway implements SelectorGateway {
 				helper.runPython(procList, req.getParams(), req.getCurValue(), req.isStartsWith(),
 						req.getFirstRecord(), req.getRecordCount());
 
-			return new ResultSelectorData(rsdData.getDataRecordList(), rsdCount.getCount());
-
+			result = new ResultSelectorData(rsdData.getDataRecordList(), rsdCount.getCount());
 		} else {
 			// единый скрипт получения данных и кол-во записей
-			ResultSelectorData result =
-				helper.runPython(procName, req.getParams(), req.getCurValue(), req.isStartsWith(),
-						req.getFirstRecord(), req.getRecordCount());
-			return result;
+			result = helper.runPython(procName, req.getParams(), req.getCurValue(),
+					req.isStartsWith(), req.getFirstRecord(), req.getRecordCount());
 		}
+
+		if ((result != null) && (result.getDataRecordList() != null)) {
+			for (DataRecord dr : result.getDataRecordList()) {
+				dr.setId(XMLUtils.xmlServiceSymbolsToNormal(dr.getId()));
+				dr.setName(XMLUtils.xmlServiceSymbolsToNormal(dr.getName()));
+				if (dr.getParameters() != null) {
+					for (String key : dr.getParameters().keySet()) {
+						dr.getParameters().put(key,
+								XMLUtils.xmlServiceSymbolsToNormal(dr.getParameters().get(key)));
+					}
+				}
+			}
+		}
+
+		return result;
 	}
 
 }
