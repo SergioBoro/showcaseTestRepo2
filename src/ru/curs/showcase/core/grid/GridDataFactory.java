83d1601e762444e372051488f4e583bd369a9273
s.borodanev <s.borodanev@curs.ru>
2017-07-19 12:13:48 +0000

    Убраны тормоза в гридах, связанные с Gettext.
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@4748 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/ru/curs/showcase/core/grid/GridDataFactory.java
+++ src/ru/curs/showcase/core/grid/GridDataFactory.java
@@ -12,8 +12,6 @@ import org.json.simple.JSONArray;
 import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
 
-import com.google.gwt.user.client.rpc.SerializationException;
-
 import ru.curs.showcase.app.api.*;
 import ru.curs.showcase.app.api.datapanel.DataPanelElementSubType;
 import ru.curs.showcase.app.api.event.*;
@@ -26,6 +24,8 @@ import ru.curs.showcase.core.sp.*;
 import ru.curs.showcase.runtime.UserDataUtils;
 import ru.curs.showcase.util.TextUtils;
 import ru.curs.showcase.util.xml.*;
+
+import com.google.gwt.user.client.rpc.SerializationException;
 
 /**
  * Фабрика для формирования данных гридов на основе XmlDS.
@@ -67,6 +67,9 @@ public class GridDataFactory extends CompBasedElementFactory {
 	private final List<GridEvent> events = new ArrayList<GridEvent>();
 
 	private GridServerState state = null;
+
+	private final ResourceBundle resourceBundleForGettext = UserDataUtils
+			.getResourceBundleForGettext();
 
 	public GridDataFactory(final RecordSetElementRawData aRaw, final GridServerState aState,
 			final boolean aApplyLocalFormatting) {
@@ -221,14 +224,17 @@ public class GridDataFactory extends CompBasedElementFactory {
 				return;
 			} else {
 				// Здесь осуществляется перевод с помощью Gettext.
-				curColId = UserDataUtils.modifyVariables(XMLUtils.unEscapeTagXml(localName));
+				curColId =
+					UserDataUtils.modifyVariables(XMLUtils.unEscapeTagXml(localName),
+							resourceBundleForGettext);
 				// curColId = XMLUtils.unEscapeTagXml(localName);
 
 				processValue = true;
 				osValue = new ByteArrayOutputStream();
 				try {
-					writerValue = XMLOutputFactory.newInstance().createXMLStreamWriter(osValue,
-							TextUtils.DEF_ENCODING);
+					writerValue =
+						XMLOutputFactory.newInstance().createXMLStreamWriter(osValue,
+								TextUtils.DEF_ENCODING);
 				} catch (XMLStreamException e) {
 					throw new SAXError(e);
 				}
@@ -252,14 +258,14 @@ public class GridDataFactory extends CompBasedElementFactory {
 		public void endElement(final String uri, final String localName, final String name) {
 			if (RECORD_TAG.equals(localName)) {
 				if (applyLocalFormatting && (rec.get(PROPS_TAG) != null)) {
-					readEvents(rec,
-							"<" + PROPS_TAG + ">" + rec.get(PROPS_TAG) + "</" + PROPS_TAG + ">");
+					readEvents(rec, "<" + PROPS_TAG + ">" + rec.get(PROPS_TAG) + "</" + PROPS_TAG
+							+ ">");
 					rec.remove(PROPS_TAG);
 				}
 
 				if ((getCallContext().getSubtype() == DataPanelElementSubType.JS_TREE_GRID)
-						&& (!(getCallContext().getPartialUpdate()
-								|| getCallContext().getUpdateParents()))) {
+						&& (!(getCallContext().getPartialUpdate() || getCallContext()
+								.getUpdateParents()))) {
 					rec.put("parentId", getCallContext().getParentId());
 				}
 
@@ -270,7 +276,9 @@ public class GridDataFactory extends CompBasedElementFactory {
 
 			if (processValue) {
 				// Здесь осуществляется перевод с помощью Gettext.
-				String colId = UserDataUtils.modifyVariables(XMLUtils.unEscapeTagXml(localName));
+				String colId =
+					UserDataUtils.modifyVariables(XMLUtils.unEscapeTagXml(localName),
+							resourceBundleForGettext);
 				// String colId = XMLUtils.unEscapeTagXml(localName);
 				try {
 					if (colId.equals(curColId)) {
@@ -285,8 +293,7 @@ public class GridDataFactory extends CompBasedElementFactory {
 								englId = curColId;
 							}
 
-							if (getCallContext()
-									.getSubtype() == DataPanelElementSubType.JS_TREE_GRID) {
+							if (getCallContext().getSubtype() == DataPanelElementSubType.JS_TREE_GRID) {
 								if ("hasChildren".equalsIgnoreCase(englId)) {
 									englId = "hasChildren";
 								}
@@ -321,8 +328,9 @@ public class GridDataFactory extends CompBasedElementFactory {
 	}
 
 	private String getTreeGridIcon(final String value) {
-		String res = String.format("%s/%s",
-				UserDataUtils.getRequiredProp(UserDataUtils.IMAGES_IN_GRID_DIR), value);
+		String res =
+			String.format("%s/%s",
+					UserDataUtils.getRequiredProp(UserDataUtils.IMAGES_IN_GRID_DIR), value);
 
 		res = "<a><img border=\"0\" src=\"" + XMLUtils.unEscapeTagXml(res) + "\"></a>";
 
@@ -337,8 +345,8 @@ public class GridDataFactory extends CompBasedElementFactory {
 	private void readEvents(final HashMap<String, String> rec, final String data) {
 		EventFactory<GridEvent> factory =
 			new EventFactory<GridEvent>(GridEvent.class, getCallContext());
-		factory.initForGetSubSetOfEvents(EVENT_COLUMN_TAG, CELL_PREFIX,
-				getElementInfo().getType().getPropsSchemaName());
+		factory.initForGetSubSetOfEvents(EVENT_COLUMN_TAG, CELL_PREFIX, getElementInfo().getType()
+				.getPropsSchemaName());
 		SAXTagHandler recPropHandler = new StartTagSAXHandler() {
 			@Override
 			public Object handleStartTag(final String aNamespaceURI, final String aLname,
@@ -406,8 +414,9 @@ public class GridDataFactory extends CompBasedElementFactory {
 		}
 
 		if (col.getValueType() == GridValueType.IMAGE) {
-			value = String.format("%s/%s",
-					UserDataUtils.getRequiredProp(UserDataUtils.IMAGES_IN_GRID_DIR), value);
+			value =
+				String.format("%s/%s",
+						UserDataUtils.getRequiredProp(UserDataUtils.IMAGES_IN_GRID_DIR), value);
 		} else if (col.getValueType() == GridValueType.LINK) {
 			value = UserDataUtils.replaceVariables(value);
 			value = normalizeLink(value);
@@ -446,8 +455,8 @@ public class GridDataFactory extends CompBasedElementFactory {
 		Matcher matcher = pattern.matcher(res);
 		res = matcher.replaceAll("&amp;");
 
-		pattern = Pattern.compile(
-				"(?<!=)(\")(?!\\s*openInNewTab)(?!\\s*text)(?!\\s*href)(?!\\s*image)(?!\\s*/\\>)");
+		pattern =
+			Pattern.compile("(?<!=)(\")(?!\\s*openInNewTab)(?!\\s*text)(?!\\s*href)(?!\\s*image)(?!\\s*/\\>)");
 		matcher = pattern.matcher(res);
 		res = matcher.replaceAll("&quot;");
 
@@ -492,8 +501,9 @@ public class GridDataFactory extends CompBasedElementFactory {
 				result = result + text;
 			} else {
 				String alt = text != null ? " alt=\"" + text + "\"" : "";
-				result = result + "<img border=\"0\" src=\"" + XMLUtils.unEscapeTagXml(image)
-						+ "\"" + alt + "/>";
+				result =
+					result + "<img border=\"0\" src=\"" + XMLUtils.unEscapeTagXml(image) + "\""
+							+ alt + "/>";
 			}
 			result = result + "</a>";
 
@@ -540,8 +550,8 @@ public class GridDataFactory extends CompBasedElementFactory {
 		return nf.format(value);
 	}
 
-	private String getStringValueOfDate(final java.util.Date date,
-			final GridServerColumnConfig col) {
+	private String
+			getStringValueOfDate(final java.util.Date date, final GridServerColumnConfig col) {
 		DateFormat df = null;
 
 		Integer style = DateFormat.DEFAULT;
