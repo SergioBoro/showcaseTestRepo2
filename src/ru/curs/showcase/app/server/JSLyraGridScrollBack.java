edd5d4320bd4a50842e2a0f83e8e4c2ce7c3023e
Boris <boris@curs.ru>
2017-12-13 13:11:07 +0000

    Сделал передачу session контекста лира-грида в форму решения при создании вебсокета для отскока ползунка
    git-svn-id: https://share.curs.ru/svn/showcase/trunk@4838 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- src/ru/curs/showcase/app/server/JSLyraGridScrollBack.java
+++ src/ru/curs/showcase/app/server/JSLyraGridScrollBack.java
@@ -11,7 +11,9 @@ import ru.curs.showcase.app.api.datapanel.*;
 import ru.curs.showcase.app.api.grid.LyraGridContext;
 import ru.curs.showcase.core.command.GeneralExceptionFactory;
 import ru.curs.showcase.core.grid.*;
-import ru.curs.showcase.util.ServletUtils;
+import ru.curs.showcase.security.SignedUsernamePasswordAuthenticationToken;
+import ru.curs.showcase.util.*;
+import ru.curs.showcase.util.xml.*;
 
 /**
  * Вебсокет для обработки обратного движения ползунка лирагрид.
@@ -41,6 +43,24 @@ public class JSLyraGridScrollBack {
 				(DataPanelElementInfo) ServletUtils.deserializeObject(stringElementInfo);
 
 			GridUtils.fillFilterContextByFilterInfo(context);
+
+			if (((WsSession) session)
+					.getUserPrincipal() instanceof SignedUsernamePasswordAuthenticationToken) {
+
+				UserAndSessionDetails usd =
+					(UserAndSessionDetails) ((SignedUsernamePasswordAuthenticationToken) ((WsSession) session)
+							.getUserPrincipal()).getDetails();
+
+				CompositeContextOnBasisOfUserAndSessionDetails contextWithSessionContext =
+					new CompositeContextOnBasisOfUserAndSessionDetails(usd);
+
+				XMLSessionContextGenerator generator =
+					new XMLSessionContextGenerator(contextWithSessionContext);
+				String sessionContext = generator.generate();
+
+				context.setSession(sessionContext);
+
+			}
 
 			LyraGridGateway lgateway = new LyraGridGateway();
 			BasicGridForm basicGridForm = lgateway.getLyraFormInstance(context, element,
